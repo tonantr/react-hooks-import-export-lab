3233bbdb8ccb37bdeb8fec52ebeeef49be69a0d1
tonantr <tonantruong@hotmail.com>
2023-05-23 15:13:56 +0700

    complete Import/Export Lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
+import * as variables from "../data/user"
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {variables.username} is a Web Developer from {variables.city}
       </h1>
     </div>
   );
 }
+
+export default Home
